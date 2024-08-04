8cdb6a717b5c4447c6197febfd469a4873066e2b
hafijurDia <hafijur@gmail.com>
2024-08-04 13:45:05 +0600

    data load by express js

--- src/pages/Shared/Rightnav/Rightnav.jsx
+++ src/pages/Shared/Rightnav/Rightnav.jsx
@@ -6,14 +6,16 @@ import ins from '../../../assets/Instagram.png'
 import swiming from '../../../assets/qZone1.png'
 import classroom from '../../../assets/qZone2.png'
 import playground from '../../../assets/qZone3.png'
+import '../Rightnav/Rightnav.css'
+import { FaGoogle, FaGithub } from "react-icons/fa6";
 
 export default function Rightnav() {
   return (
     <div>
       <div className='mb-5'>
       <p className="fs-5 fw-bold">Login With</p>
-      <Button className="btn w-100 border-primary bg-white text-primary mb-2">Login with Google</Button>
-      <Button className="btn w-100 border-secondary bg-white text-secondary">Login with Github</Button>
+      <Button className="btn w-100 border-primary bg-white text-primary mb-2"><FaGoogle /> Login with Google</Button>
+      <Button className="btn w-100 border-secondary bg-white text-secondary"><FaGithub /> Login with Github</Button>
       </div>
       <div className='mb-5'>
       <p className="fs-5 fw-bold">Find Us On</p>
@@ -30,11 +32,10 @@ export default function Rightnav() {
       <img className='mb-3 w-100' src={playground} alt="" /> 
       
       </div>
-      <div className='mb-5'>
-      <p className="fs-5 fw-bold pt-3 px-3">Q-Zone</p>
-      <img className='mb-3 w-100' src={swiming} alt="" /> 
-      <img className='mb-3 w-100' src={classroom} alt="" /> 
-      <img className='mb-3 w-100' src={playground} alt="" /> 
+      <div className='mb-5 sidebanner text-center px-4 py-5'>
+      <h3 className='text-white'>Create an Amazing Newspaper</h3>
+      <p className='text-white'>Discover thousands of options, easy to customize layouts, one-click to import demo and much more.</p>
+      <button className='btn btn-danger'>Learn More</button>
       
       </div>
     </div>
