87e085ae3e3dad82b40a8eb91ac470adb2a83e3d
hafijurDia <hafijur@gmail.com>
2024-08-10 11:38:55 +0600

    letf menu UI done

--- src/pages/Shared/Leftnav/Leftnav.jsx
+++ src/pages/Shared/Leftnav/Leftnav.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import post1 from '../../../assets/1.png';
+import moment from 'moment';
 
 export default function Leftnav() {
   const [categories, setCategories] = useState([]);
@@ -6,12 +9,63 @@ export default function Leftnav() {
   useEffect(()=>{
     fetch('http://localhost:5000/categories')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => setCategories(data))
     .catch(error => console.log(error))
   },[])
   return (
     <div>
-      <p>left nav</p>
+      <div className='mb-5'>
+      <p className="fs-5 fw-bold">All Category</p>
+      <ul className='p-0'>
+        {
+          categories.map(category => <li
+          key={category.id} className='list-group-item py-2'
+          >
+            <Link to={`/category/${category.id}`} className='text-decoration-none text-secondary'>{category.name}</Link>
+          </li>)
+        }
+      </ul>
+      </div>
+      <div className='mb-2'>
+        <div>
+          <img className='w-100' src={post1} alt="" />
+          <p className='fw-medium my-2'>Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+          <div className='d-flex justify-content-between'>
+            <p className='fs-6'><small>Sports</small></p>
+            <p className='fs-6'><small>{moment().format("MMM D, YYYY")}</small></p>
+          </div>
+        </div>
+      </div>
+      <div className='mb-2'>
+        <div>
+          <img className='w-100' src={post1} alt="" />
+          <p className='fw-medium my-2'>Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+          <div className='d-flex justify-content-between'>
+          <p className='fs-6'><small>Sports</small></p>
+          <p className='fs-6'><small>{moment().format("MMM D, YYYY")}</small></p>
+          </div>
+        </div>
+      </div>
+      <div className='mb-2'>
+        <div>
+          <img className='w-100' src={post1} alt="" />
+          <p className='fw-medium my-2'>Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+          <div className='d-flex justify-content-between'>
+          <p className='fs-6'><small>Sports</small></p>
+          <p className='fs-6'><small>{moment().format("MMM D, YYYY")}</small></p>
+          </div>
+        </div>
+      </div>
+      <div className='mb-2'>
+        <div>
+          <img className='w-100' src={post1} alt="" />
+          <p className='fw-medium my-2'>Bayern Slams Authorities Over Flight Delay to Club World Cup</p>
+          <div className='d-flex justify-content-between'>
+          <p className='fs-6'><small>Sports</small></p>
+          <p className='fs-6'><small>{moment().format("MMM D, YYYY")}</small></p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
